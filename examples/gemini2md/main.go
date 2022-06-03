22cba52b1f883ad4f481d91e9787122a65205cda
shrmpy <>
2022-06-03 16:54:17 -0700

    ebitengine name

--- examples/gemini2md/main.go
+++ examples/gemini2md/main.go
@@ -15,16 +15,14 @@ import "github.com/shrmpy/gmi"
 
 func main() {
 	var (
-		aer error
-		abs string
-		dir = flag.String("dir", "", "Work directory with Gem files")
+		errp error
+		abs  string
+		dir  = flag.String("dir", "", "Work directory with Gemtext files")
 	)
 	flag.Parse()
-
-	if abs, aer = filepath.Abs(*dir); aer != nil {
-		log.Fatalf("Unknown path, %v", aer)
+	if abs, errp = filepath.Abs(*dir); errp != nil {
+		log.Fatalf("Unknown path, %v", errp)
 	}
-
 	var ctrl = gmi.NewControl(context.Background())
 	ctrl.Attach(gmi.LinkLine, rewriteLink)
 	ctrl.Attach(gmi.PlainLine, rewritePlain)
@@ -41,37 +39,42 @@ func main() {
 			log.Printf("INFO non .gmi skipped, %s", d.Name())
 			return nil
 		}
-
-		file, fer := os.Open(path)
-		if fer != nil {
-			log.Printf("ERROR file, %v", fer)
-			return fer
+		var (
+			errs  error
+			rdr   *bufio.Reader
+			ifile *os.File
+			outf  string
+			md    string
+		)
+		if ifile, errs = os.Open(path); errs != nil {
+			log.Printf("ERROR file, %v", errs)
+			return errs
 		}
-		defer file.Close()
-		log.Printf("INFO reading, %v", d.Name())
-		var rdr = bufio.NewReader(file)
+		defer ifile.Close()
+		log.Printf("INFO reading, %s", d.Name())
+		rdr = bufio.NewReader(ifile)
 
-		md, wer := ctrl.Retrieve(rdr)
-		if wer != nil {
-			log.Printf("ERROR Retrieve, %v", wer)
-			return wer
+		if md, errs = ctrl.Retrieve(rdr); errs != nil {
+			log.Printf("ERROR Retrieve, %v", errs)
+			return errs
 		}
-
-		var ofile = fmt.Sprintf("%s.md", strings.TrimSuffix(path, ".gmi"))
-		log.Printf("INFO writing, %s : %d", ofile, len(md))
-		var oerr = os.WriteFile(ofile, []byte(md), d.Type())
-		if oerr != nil {
-			log.Printf("ERROR output file, %v", oerr)
-			return oerr
+		outf = fmt.Sprintf("%s.md", strings.TrimSuffix(path, ".gmi"))
+		log.Printf("INFO writing, %s : %d", outf, len(md))
+		errs = os.WriteFile(outf, []byte(md), d.Type())
+		if errs != nil {
+			log.Printf("ERROR output file, %v", errs)
+			return errs
 		}
 
 		return nil
 	})
 }
 func rewriteLink(n gmi.Node) string {
-	var lnk = n.(*gmi.LinkNode)
-	var name = lnk.Friendly
-	var lu = lnk.URL
+	var (
+		lnk  = n.(*gmi.LinkNode)
+		name = lnk.Friendly
+		lu   = lnk.URL
+	)
 	if name == "" {
 		name = lu.String()
 	}
--- go.mod
+++ go.mod
@@ -6,8 +6,8 @@ require (
 	github.com/gdamore/tcell/v2 v2.5.1
 	github.com/hajimehoshi/ebiten/v2 v2.3.3
 	github.com/tinne26/etxt v0.0.1
-	golang.org/x/crypto v0.0.0-20210921155107-089bfa567519
-	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c
+	golang.org/x/crypto v0.0.0-20220525230936-793ad666bf5e
+	golang.org/x/sync v0.0.0-20220601150217-0de741cfad7f
 )
 
 require (
@@ -21,7 +21,7 @@ require (
 	golang.org/x/exp v0.0.0-20190731235908-ec7cb31e5a56 // indirect
 	golang.org/x/image v0.0.0-20220321031419-a8550c1d254a // indirect
 	golang.org/x/mobile v0.0.0-20220518205345-8578da9835fd // indirect
-	golang.org/x/sys v0.0.0-20220408201424-a24fb2fb8a0f // indirect
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a // indirect
 	golang.org/x/term v0.0.0-20201210144234-2321bbc49cbf // indirect
 	golang.org/x/text v0.3.7 // indirect
 )
